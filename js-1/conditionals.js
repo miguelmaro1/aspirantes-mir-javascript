8fa73f60089fc4e254e4a1751b6e4902a345af35
Miguel Malagón <78055087+miguelmaro1@users.noreply.github.com>
2023-05-10 22:05:48 -0500

    Create conditionals.js

---
+++ js-1/conditionals.js
@@ -0,0 +1,19 @@
+// Ejericio colores
+
+function color(num){
+    if(num==1){
+        return "The color is black"
+    }else{
+        if(num==2){
+            return "The color is white"
+        }else{
+            if(num==3){
+                return "The color is red"
+            }else{
+                return "The color is green"
+            }
+        }
+    }
+}
+
+console.log(color(4));
